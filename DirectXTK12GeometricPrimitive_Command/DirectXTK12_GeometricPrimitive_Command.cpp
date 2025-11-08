c048e917d982ab9ff69886a6b9c6af98ef93d55b
Kanta yukawa <hattendosummer@yahoo.co.jp>
2025-11-08 15:35:04 +0900

    DirectXTK12のGeometricPrimiticeプロジェクト立ち上げ

---
+++ DirectXTK12GeometricPrimitive_Command/DirectXTK12_GeometricPrimitive_Command.cpp
@@ -0,0 +1,253 @@
+#include "pch.h"
+#include "DirectXTK12_GeometricPrimitive_Command.h"
+#include "pch.h"
+#include <ResourceUploadBatch.h>
+#include "DirectXTK12Polygon.h"
+#include <d3dcompiler.h>
+#include <d3dx12.h>
+#include <GeometricPrimitive.h>
+class DirectX::GeometricPrimitive;
+enum Descriptors
+{
+    WindowsLogo,
+    CourierFont,
+    ControllerFont,
+    GamerPic,
+    Count
+};
+DirectX::GeometricPrimitive::VertexCollection vertices;
+DirectX::GeometricPrimitive::IndexCollection indices;
+DirectX::SharedGraphicsResource vb;
+DirectX::SharedGraphicsResource ib;
+HRESULT DirectXTK12Polygon::CreateBuffer(DirectX::GraphicsMemory* graphicsmemory, DX::DeviceResources* deviceResources, int height, int width)
+{
+    // Create shape data
+
+    DirectX::GeometricPrimitive::CreateSphere(vertices, indices);
+
+
+    // Place data on upload heap
+    size_t vsize = vertices.size() * sizeof(DirectX::VertexPositionColor);
+    vb = DirectX::GraphicsMemory::Get().Allocate(vsize);
+    memcpy(vb.Memory(), vertices.data(), vsize);
+
+    size_t isize = indices.size() * sizeof(uint16_t);
+    ib = DirectX::GraphicsMemory::Get().Allocate(isize);
+    memcpy(ib.Memory(), indices.data(), isize);
+    /*
+    // 三角形の頂点データ
+
+    vertices.resize(3);
+
+    vertices[0].position.x = 0.0f;
+    vertices[0].position.y = 0.5f;
+    vertices[0].position.z = 0.0f;
+
+    vertices[1].position.x = 0.5f;
+    vertices[1].position.y = -0.5f;
+    vertices[1].position.z = 0.0f;
+
+    vertices[2].position.x = -0.5f;
+    vertices[2].position.y = -0.5f;
+    vertices[2].position.z = 0.0f;
+    indices.resize(3);
+
+    indices[0] = 0;
+    indices[1] = 1;
+    indices[2] = 2;
+    */
+    DirectX::ResourceUploadBatch resourceUpload(deviceResources->GetD3DDevice());
+
+    resourceUpload.Begin();
+
+    /*
+    m_VertexBuffer = graphicsmemory->Allocate(sizeof(DirectX::VertexPosition) * vertices.size());
+    memcpy(m_VertexBuffer.Memory(), vertices.data(), sizeof(DirectX::VertexPosition) * vertices.size());
+    m_IndexBuffer = graphicsmemory->Allocate(sizeof(unsigned short) * indices.size());
+    //(DirectXTK12Assimpで追加)
+    */
+    //m_vertexBufferView.BufferLocation = m_VertexBuffer.GpuAddress();
+    m_vertexBufferView.BufferLocation = vb.GpuAddress();
+    //m_vertexBufferView.StrideInBytes = sizeof(DirectX::VertexPosition);
+    m_vertexBufferView.StrideInBytes = sizeof(DirectX::VertexPositionColor);
+    // m_vertexBufferView.SizeInBytes = static_cast<UINT>(m_VertexBuffer.Size());
+    m_vertexBufferView.SizeInBytes = static_cast<UINT>(vb.Size());
+
+    //m_indexBufferView.BufferLocation = m_IndexBuffer.GpuAddress();
+    m_indexBufferView.BufferLocation = ib.GpuAddress();
+    m_indexBufferView.Format = DXGI_FORMAT_R16_UINT;
+    // m_indexBufferView.SizeInBytes = m_IndexBuffer.Size();
+    m_indexBufferView.SizeInBytes = ib.Size();
+    //memcpy(m_IndexBuffer.Memory(), indices.data(), sizeof(unsigned short) * indices.size());
+    memcpy(ib.Memory(), indices.data(), sizeof(uint16_t) * indices.size());
+
+
+
+    //定数バッファの作成(DIrectXTK12Assimpで追加)
+
+    //https://github.com/microsoft/DirectXTK12/wiki/GraphicsMemory
+
+    m_pipelineState = CreateGraphicsPipelineState(deviceResources, L"VertexShader.hlsl", L"PixelShader.hlsl");
+
+    // リソースのアップロードを終了
+    auto uploadResourcesFinished = resourceUpload.End(deviceResources->GetCommandQueue());
+    return S_OK;
+}
+
+
+//(DIrectXTK12Assimpで追加)
+void DirectXTK12Polygon::Draw(const DX::DeviceResources* DR) {
+
+
+    DirectX::ResourceUploadBatch resourceUpload(DR->GetD3DDevice());
+
+    resourceUpload.Begin();
+
+    if (vertices.empty() || indices.empty()) {
+        OutputDebugStringA("Vertices or indices buffer is empty.\n");
+        return;
+    }
+
+
+    auto commandList = DR->GetCommandList();
+    auto renderTarget = DR->GetRenderTarget();
+    if (!commandList) {
+        OutputDebugStringA("Command list is null.\n");
+        return;
+    }
+
+    // 入力アセンブラー設定
+    commandList->IASetIndexBuffer(&m_indexBufferView);
+    commandList->IASetVertexBuffers(0, 1, &m_vertexBufferView);
+    commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+
+
+    // ルートシグネチャ設定
+    commandList->SetGraphicsRootSignature(m_rootSignature.Get());
+
+
+    // パイプラインステート設定
+    commandList->SetPipelineState(m_pipelineState.Get());
+
+    // 描画コール
+    commandList->DrawIndexedInstanced(
+        static_cast<UINT>(indices.size()), // インデックス数
+        1,                                 // インスタンス数
+        0,                                 // 開始インデックス
+        0,                                 // 頂点オフセット
+        0                                  // インスタンスオフセット
+    );
+
+
+
+    auto uploadResourcesFinished = resourceUpload.End(
+        DR->GetCommandQueue());
+
+    uploadResourcesFinished.wait();
+}
+
+using Microsoft::WRL::ComPtr;
+//(DIrectXTK12Assimpで追加)
+// グラフィックパイプラインステートを作成する関数
+Microsoft::WRL::ComPtr<ID3D12PipelineState> DirectXTK12Polygon::CreateGraphicsPipelineState(
+    DX::DeviceResources* deviceresources,
+
+    const std::wstring& vertexShaderPath,
+    const std::wstring& pixelShaderPath)
+{
+    // シェーダーをコンパイル
+    ComPtr<ID3DBlob> vertexShader;
+    ComPtr<ID3DBlob> pixelShader;
+    ComPtr<ID3DBlob> errorBlob;
+    DirectX::RenderTargetState rtState(DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_FORMAT_D32_FLOAT);
+    HRESULT hr = D3DCompileFromFile(
+        vertexShaderPath.c_str(),
+        nullptr,
+        nullptr,
+        "main", // エントリーポイント
+        "vs_5_0", // シェーダーモデル
+        0,
+        0,
+        &vertexShader,
+        &errorBlob
+    );
+
+    if (FAILED(hr)) {
+        if (errorBlob) {
+            OutputDebugStringA((char*)errorBlob->GetBufferPointer());
+        }
+        throw std::runtime_error("Failed to compile vertex shader");
+    }
+
+    hr = D3DCompileFromFile(
+        pixelShaderPath.c_str(),
+        nullptr,
+        nullptr,
+        "main",
+        "ps_5_0",
+        0,
+        0,
+        &pixelShader,
+        &errorBlob
+    );
+
+    if (FAILED(hr)) {
+        if (errorBlob) {
+            OutputDebugStringA((char*)errorBlob->GetBufferPointer());
+        }
+        throw std::runtime_error("Failed to compile pixel shader");
+    }
+
+    // 入力レイアウトを定義
+    m_layout = {
+        { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+
+    };
+
+    D3D12_ROOT_SIGNATURE_FLAGS rootSignatureFlags =
+        D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT |
+        D3D12_ROOT_SIGNATURE_FLAG_DENY_DOMAIN_SHADER_ROOT_ACCESS |
+        D3D12_ROOT_SIGNATURE_FLAG_DENY_GEOMETRY_SHADER_ROOT_ACCESS |
+        D3D12_ROOT_SIGNATURE_FLAG_DENY_HULL_SHADER_ROOT_ACCESS;
+
+
+    // Root parameter descriptor
+    CD3DX12_ROOT_SIGNATURE_DESC rsigDesc = {};
+
+    // use all parameters
+    rsigDesc.Init(0, nullptr, 0, nullptr, rootSignatureFlags);
+
+    DX::ThrowIfFailed(DirectX::CreateRootSignature(deviceresources->GetD3DDevice(), &rsigDesc, m_rootSignature.ReleaseAndGetAddressOf()));
+
+    //https://github.com/microsoft/DirectXTK12/wiki/PSOs,-Shaders,-and-Signatures
+    // 
+    // 
+    D3D12_INPUT_LAYOUT_DESC inputlayaout = { m_layout.data(), m_layout.size() };
+    DirectX::EffectPipelineStateDescription pd(
+        &inputlayaout,
+        DirectX::CommonStates::Opaque,
+        DirectX::CommonStates::DepthDefault,
+        DirectX::CommonStates::CullCounterClockwise,
+        rtState);
+    D3D12_SHADER_BYTECODE vertexshaderBCode = { vertexShader->GetBufferPointer(), vertexShader->GetBufferSize() };
+    //https://learn.microsoft.com/ja-jp/windows/win32/direct3d12/cd3dx12-shader-bytecode
+
+    D3D12_SHADER_BYTECODE pixelShaderBCode = { pixelShader->GetBufferPointer(), pixelShader->GetBufferSize() };
+    // パイプラインステートオブジェクトを作成
+    ComPtr<ID3D12PipelineState> pipelineState;
+    pd.CreatePipelineState(
+        deviceresources->GetD3DDevice(),
+        m_rootSignature.Get(),
+        vertexshaderBCode,
+
+        pixelShaderBCode,
+
+        pipelineState.GetAddressOf()
+    );
+    if (FAILED(hr)) {
+        throw std::runtime_error("Failed to create pipeline state");
+    }
+
+    return pipelineState;
+}
---
+++ DirectXTK12GeometricPrimitive_Command/DirectXTK12_GeometricPrimitive_Command.h
@@ -0,0 +1,46 @@
+#pragma once
+class DirectXTK12_GeometricPrimitive_Command
+{
+};
+
+#pragma once
+#include "pch.h"
+#include <DeviceResources.h>
+#include <DescriptorHeap.h>
+#include <memory>
+#include <GraphicsMemory.h>
+class DirectXTK12Polygon
+{
+public:
+	HRESULT CreateBuffer(DirectX::GraphicsMemory* graphicsmemory, DX::DeviceResources* deviceResources, int height, int width);
+
+
+	std::unique_ptr<DirectX::DescriptorHeap> m_resourceDescriptors;
+
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> CreateGraphicsPipelineState(DX::DeviceResources* deviceresources, const std::wstring& vertexShaderPath, const std::wstring& pixelShaderPath);
+
+	void Draw(const DX::DeviceResources* DR);
+
+
+	D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView;
+	D3D12_INDEX_BUFFER_VIEW m_indexBufferView;
+	Microsoft::WRL::ComPtr<ID3D12RootSignature> m_rootSignature;
+
+
+
+	//バッファ
+
+	DirectX::GraphicsResource m_VertexBuffer;
+	DirectX::GraphicsResource m_IndexBuffer;
+	std::vector<D3D12_INPUT_ELEMENT_DESC> m_layout;
+	//std::vector<DirectX::VertexPosition> vertices;
+	//std::vector<unsigned short> indices;
+
+	DirectX::XMMATRIX modelmat;
+	//シェーダーの作成
+	Microsoft::WRL::ComPtr<ID3DBlob> vertexShader;//新規追加
+	Microsoft::WRL::ComPtr<ID3DBlob> pixelShader;//新規追加
+	DirectX::GraphicsResource SceneCBResource;//新規追加
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> m_pipelineState;//新規追加
+};
+
